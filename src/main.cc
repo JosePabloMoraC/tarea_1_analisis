3b1036fab40ceaaaff11a2671252cdc2a44a0718
VALERIA RODRIGUEZ RAMIREZ <valeria.rodriguez21@ucr.ac.cr>
2023-05-21 19:59:46 -0600

    Submenu cola

--- src/main.cc
+++ src/main.cc
@@ -6,6 +6,7 @@ using namespace std;
 void submenuCola(){
   int respuestaC;
   bool stop = false;
+  Cola c1 = Cola();
   cout <<  "****  Cola  ***" << endl;
   cout <<  "1) Iniciar" << endl;
   cout <<  "2) Agregar" << endl;
@@ -13,26 +14,46 @@ void submenuCola(){
   cout <<  "4) Frente" << endl;
   cout <<  "5) Vacia" << endl;
   cout <<  "6) Vaciar" << endl;
-  cout <<  "7) Salir" << endl;
+  cout <<  "7) Destruir" << endl;
+  cout <<  "8) Salir" << endl;
   cout << "Por favor utilice sólo los números dados según lo que desee hacer" << endl;
   while (stop != true) {
     cout << "Ingrese su respuesta: " << endl;
     cin >> respuestaC;
     switch (respuestaC) {
       case 1: cout << "Ha seleccionado iniciar" << endl;
-      break;
+        c1 = Cola();
+        break;
       case 2: cout << "Ha seleccionado agregar" << endl;
+        cout << "Digite el elemento a agregar: " << endl;
+        int element;
+        cin >> element;
+        c1.agregar(element);
+        cout << "Se agregó el elemento " << element << " a la cola." << endl;
       break;
       case 3: cout << "Ha seleccionado sacar" << endl;
-      break;
+        cout << "Se sacó el elemento " << c1.sacar() << " de la cola." << endl;
+        break;
       case 4: cout << "Ha seleccionado frente" << endl;
-      break;
+        cout << "El elemento del frente es " << c1.frente() << endl;
+        break;
       case 5: cout << "Ha seleccionado vacia" << endl;
-      break;
+        if (c1.vacia()) {
+          cout << "La cola está vacía" << endl;
+        } else {
+          cout << "La cola no está vacía" << endl;
+        }
+        break;
       case 6: cout << "Ha seleccionado vaciar" << endl;
-      break;
-      case 7: cout << "Ha seleccionado salir" << endl, stop = true;
-      break;
+        c1.vaciar();
+        cout << "Se ha vaciado la cola" << endl;
+        break;
+      case 7: cout << "Ha seleccionado destruir" << endl;
+        c1.~Cola();
+        cout << "Se ha destruido la cola" << endl;
+        break;
+      case 8: cout << "Ha seleccionado salir" << endl, stop = true;
+        break;
       default: cout << "Respuesta inválida" << endl, stop = true;
     }
   }
