dda711788ecca559c238b148a1cbc7c58ac230cf
KyaraMR <87711179+KyaraMR@users.noreply.github.com>
2023-06-03 10:54:43 -0600

    Update algoritmos.hh

--- include/algoritmos.hh
+++ include/algoritmos.hh
@@ -11,6 +11,12 @@ void etiquetasNivel(int nivel, Arbol& a);
 int nivelesArbol(Arbol& a);
 void nivelesArbolRecursivo(nodo_t nodoActual, int nivelActual, Arbol& a, int& nNiveles);
 
+bool etiquetasRepetidas(Arbol& a);
+
+void copiar(Arbol& a, Arbol& b);
+
+bool iguales(Arbol& a, Arbol& b);
+
 int profundidadNodo(nodo_t n, Arbol& a);
 void profundidadRecursivo(nodo_t nodoBuscado, nodo_t nodoActual, int profundidadActual, Arbol& a, int& profundidad);
 
